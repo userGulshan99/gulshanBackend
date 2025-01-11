a5e0ee4f703dbf8a07a1c7e10a8a0bcd98fae5de
Gulshan <bankargulshan@gmail.com>
2025-01-11 06:38:51 +0530

    create backend to user log in

--- ET/backend/controllers/users.controllers.js
+++ ET/backend/controllers/users.controllers.js
@@ -30,6 +30,38 @@ const postUser = async (req, res, next) =>{
 
 }
 
+const getUser = async (req, res, next) =>{
+  try {
+      const {email, password} = req.body;
+      
+      if(!email || !password){
+        return res.status(400).json({'Error' : 'All fields are required'})
+      }
+    
+      let user = await User.findOne({
+        where:{
+            email:email
+        }
+      });
+    
+      if(!user){
+        return res.status(404).json({'message' : 'User Not Found!'});
+      }
+
+      if(user.password !== password){
+        return res.status(401).json({'Error' : 'Password does not match'});
+      }
+
+      return res.status(200).json({'message' : 'User logged in successfully'});
+
+    } catch (error) {
+      return res.status(500).json({'Error' : 'Internal Server Error'});
+    }
+  
+}
+
+
 module.exports = {
-  postUser
+  postUser,
+  getUser
 };
--- ET/backend/routes/users.routes.js
+++ ET/backend/routes/users.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const userController = require('../controllers/users.controllers.js');
 
 router.post('/user/signup', userController.postUser);
+router.post('/user/login', userController.getUser);
 
 module.exports = router;
